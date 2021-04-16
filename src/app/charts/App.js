da9e8151817aa1a1ce94c64a6d4cba4df69fbe32
Geremsa <geremsa111@gmail.com>
2021-04-16 17:38:59 +0530

    replaced with highcharts react official

--- src/app/charts/App.js
+++ src/app/charts/App.js
@@ -1,27 +1,26 @@
-import {useState} from 'react'
 import 'antd/dist/antd.css';
-import ReactHighcharts from "react-highcharts";
-import highcharts3d from "highcharts/highcharts-3d";
+import {useState} from 'react';
+import HighchartsReact from 'highcharts-react-official';
+import Highcharts from 'highcharts';
 import {Layout, Menu} from 'antd';
+import './app.css';
 import {
   DesktopOutlined,
   PieChartOutlined,
-  FileOutlined,
-  TeamOutlined,
-  UserOutlined,
+  FileOutlined
 } from '@ant-design/icons';
-const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
-highcharts3d(ReactHighcharts.Highcharts);
+const {  Content, Footer, Sider } = Layout;
+
 const config = {
   chart: {
-    type: "column",
+    type: "area",
     options3d: {
       enabled: false,
       alpha: 10,
     },
     backgroundColor:"#E0E1DD",
   },
+  colors:["#8F83DA","#A040A0"],
   credits:{
     enabled:false
   },
@@ -32,25 +31,67 @@ const config = {
     text: "3D donut in Highcharts",
   },
   plotOptions: {
-    pie: {
-      innerSize: 100,
-      depth: 45,
-    },
+    area: {
+      stacking: 'normal',
+        lineColor: '#666666',
+        lineWidth: 1,
+        marker: {
+            lineWidth: 1,
+            lineColor: '#666666'
+        }
+    }
+},
+  xAxis:{
+    title:{
+      text:''
+    }
   },
-  colors:["#415A77"],
+  yAxis:{
+    title:{
+      text:'Amount'
+    }
+  },
+  responsive:{
+   rules:[
+     {
+       condition:{
+         maxWidth:500
+       },
+       chartOptions:{
+         yAxis:{title:{text:''}},
+         subtitle:{text:''},
+         legend:{enabled:false}
+       }
+     }
+   ]
+  },
   series: [
     {
       name: "Delivered amount",
       data: [
         ["Bananas", 8],
-        ["Kiwi", 3],
+        ["Kiwi", 6],
         ["Mixed nuts", 1],
         ["Oranges", 6],
         ["Apples", 8],
-        ["Pears", 4],
+        ["Pears", 9],
         ["Clementines", 4],
         ["Reddish (bag)", 1],
         ["Grapes (bunch)", 1],
+      ],
+    },
+    {
+      name: "Produced amount",
+      data: [
+        ["Bananas", 10],
+        ["Kiwi", 5],
+        ["Mixed nuts", 1],
+        ["Oranges", 9],
+        ["Apples", 7],
+        ["Pears", 6],
+        ["Clementines", 10],
+        ["Reddish (bag)", 12],
+        ["Grapes (bunch)", 14],
       ],
     },
   ],
@@ -64,33 +105,23 @@ function App() {
   return <div id="box">
    <Layout style={{ minHeight: '100vh' }}>
         <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
-          <div className="logo" />
+          <div className="logo" >D</div>
           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1" icon={<PieChartOutlined />}>
-              Option 1
+            <Menu.Item key="1" icon={<PieChartOutlined />} className="itemcustom">
+             Analytics
             </Menu.Item>
-            <Menu.Item key="2" icon={<DesktopOutlined />}>
-              Option 2
+            <Menu.Item key="2" icon={<DesktopOutlined />} className="itemcustom">
+              Dashboard
             </Menu.Item>
-            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-              <Menu.Item key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-              <Menu.Item key="6">Team 1</Menu.Item>
-              <Menu.Item key="8">Team 2</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="9" icon={<FileOutlined />}>
+            <Menu.Item key="9" icon={<FileOutlined />} className="itemcustom">
               Files
             </Menu.Item>
           </Menu>
         </Sider>
         <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }} />
-          <Content style={{ margin: '0 16px' }}>
-            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-            <ReactHighcharts config={config}/>
+          <Content style={{ margin: '0 16px' }} className="content-container">
+            <div  style={{ padding: 24,height:'50%' }}>
+            <HighchartsReact  highcharts={Highcharts} options={config} containerProps={{className:"content"}}/>
             </div>
           </Content>
           <Footer style={{ textAlign: 'center' }}>Gdai ©2021 </Footer>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app/pexels/App';
+import App from './app/charts/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
